277f67835c3a34ca00c1aa506a6b8b6300cc548f
David Serrano <david-serrano@outlook.com>
2025-02-25 19:59:13 -0500

    Fix image paths by importing them directly

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 import '../styles/App.css';
+import img1 from '../assets/img-1.png'; // Importa las imágenes
+import img2 from '../assets/img-2.png';
+import img3 from '../assets/img-3.png';
+import img4 from '../assets/img-4.png';
+import img5 from '../assets/img-5.png';
 
 const Projects = () => {
   const [activeProject, setActiveProject] = useState(null);
@@ -11,7 +16,7 @@ const Projects = () => {
       description:
         'Find Remote Jobs is a web application built with React and TypeScript that allows users to search and explore remote job opportunities. The app offers an intuitive and highly interactive interface, featuring functionalities such as job search, job listings, detailed descriptions, pagination, bookmarks, and the ability to view recent and important jobs.',
       techs: ['React', 'Typescript', 'Javascript', 'HTML', 'CSS'],
-      image: '/src/assets/img-1.png',
+      image: img1,
       github: 'https://github.com/DavidSerranoReyes/RMTDEV-TS',
       demo: 'https://rmtdev-ts.vercel.app/',
     },
@@ -21,7 +26,7 @@ const Projects = () => {
       description:
         'CorpComment is a web application that allows customers to leave reviews about companies and suggestions for their products. This platform aims to provide an easy and quick way for users to express their opinions on businesses and products, helping others make informed decisions',
       techs: ['React', 'Typescript', 'Vite', 'Zustand', 'Tailwind CSS'],
-      image: '/src/assets/img-2.png',
+      image: img2,
       github: 'https://github.com/DavidSerranoReyes/Corp-Comment-app',
       demo: 'https://corp-comment-app.vercel.app/',
     },
@@ -31,7 +36,7 @@ const Projects = () => {
       description:
         "Word Analytics is an application designed to help you analyze and optimize your content. It is a useful tool for anyone who wants to fine-tune their content before posting on platforms like X (formerly Twitter), Instagram, and TikTok, ensuring it meets each platform's character limits. ",
       techs: ['JavaScript', 'CSS', 'HTML'],
-      image: '/src/assets/img-3.png',
+      image: img3,
       github: 'https://github.com/DavidSerranoReyes/WordAnalytics',
       demo: 'https://word-analytics-smoky.vercel.app/',
     },
@@ -41,7 +46,7 @@ const Projects = () => {
       description:
         'Dynamic Counter is a simple web application that allows users to interact with a counter. The counter can be incremented or decremented by pressing the + and - keys on the keyboard. Additionally, the user can press the Refresh button to reset the counter to zero. There is a predefined limit that the counter cannot exceed, providing a controlled range for interaction.',
       techs: ['React', 'JavaScript', 'CSS', 'HTML'],
-      image: '/src/assets/img-4.png',
+      image: img4,
       github: 'https://github.com/DavidSerranoReyes/FancyCounter',
       demo: 'https://fancy-counter-topaz.vercel.app/',
     },
@@ -51,7 +56,7 @@ const Projects = () => {
       description:
         'A modern, interactive landing page designed and developed for a foundation. This project showcases a responsive, user-friendly interface with engaging features and optimized performance.',
       techs: ['JavaScript', 'CSS3', 'HTML', 'Bootstrap'],
-      image: '/src/assets/img-5.png',
+      image: img5,
       github: 'https://github.com/DavidSerranoReyes/fadeps',
       demo: 'https://fadeps.vercel.app/',
     },
